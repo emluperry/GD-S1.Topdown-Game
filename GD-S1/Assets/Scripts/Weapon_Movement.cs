46cc8409265b95fd12b464d0703b769d9bd489e2
emluperry <emluperry@gmail.com>
2022-10-31 15:33:53 +0000

    Basic Weapon Movement

--- GD-S1/Assets/Scripts/Weapon_Movement.cs
+++ GD-S1/Assets/Scripts/Weapon_Movement.cs
@@ -7,19 +7,24 @@ public class Weapon_Movement : MonoBehaviour
     private Rigidbody2D m_RB;
 
     private Vector2 m_InputDirection;
+    private Vector2 m_GoalVelocity;
+    private Vector2 m_PrevLocation;
+    private float m_DistTravelled;
 
-    [Header("Weapon Motion")]
+    [Header("Weapon Limits")]
     [SerializeField][Min(0f)] private float m_MaxSpeed = 10f;
     [SerializeField][Min(0f)] private float m_MaxDistance = 4f;
-
-    [SerializeField][Min(0f)] private float m_Acceleration = 200f;
     [SerializeField][Min(0f)] private float m_MaxAccelerationForce = 150f;
-    [SerializeField][Min(0f)] private AnimationCurve m_AccelerationCurve;
     [SerializeField][Min(0f)] private AnimationCurve m_MaxAccelerationCurve;
+
+    [Header("Weapon Movement")]
+    [SerializeField][Min(0f)] private float m_Acceleration = 200f;
+    [SerializeField][Min(0f)] private AnimationCurve m_AccelerationCurve;
 
     private void Start()
     {
         m_RB = GetComponent<Rigidbody2D>();
+        m_DistTravelled = 0;
     }
 
     private void Update()
@@ -34,6 +39,30 @@ public class Weapon_Movement : MonoBehaviour
 
     private void ApplyMovement()
     {
+        //has current position. determine distance moved since last called
+        float deltaDistance = (m_PrevLocation - new Vector2(transform.position.x, transform.position.y)).magnitude;
+        //add distance to accumulative dist
+        m_DistTravelled += deltaDistance;
+        //if greater than max distance, return to player. else continue
+        if(m_DistTravelled > m_MaxDistance)
+        {
+            m_RB.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            float velDot = Vector2.Dot(m_InputDirection, m_GoalVelocity.normalized);
+            float acceleration = m_Acceleration * m_AccelerationCurve.Evaluate(velDot);
 
+            m_GoalVelocity = Vector2.MoveTowards(m_GoalVelocity, m_InputDirection * m_MaxSpeed, acceleration * Time.fixedDeltaTime);
+
+            Vector2 NeededAcceleration = (m_GoalVelocity - new Vector2(m_RB.velocity.x, m_RB.velocity.y)) / Time.fixedDeltaTime;
+
+            float MaxAcceleration = m_MaxAccelerationForce * m_MaxAccelerationCurve.Evaluate(velDot);
+
+            NeededAcceleration = Vector3.ClampMagnitude(NeededAcceleration, MaxAcceleration);
+
+            m_PrevLocation = transform.position;
+            m_RB.AddForce(NeededAcceleration, ForceMode2D.Force);
+        }
     }
 }
