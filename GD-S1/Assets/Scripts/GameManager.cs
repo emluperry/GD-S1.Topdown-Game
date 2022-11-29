2059d2d363bb5fff4dc428ba89b16c9f49c3bd90
emluperry <emluperry@gmail.com>
2022-11-29 20:57:36 +0000

    Minor Scene Edits

--- GD-S1/Assets/Scripts/GameManager.cs
+++ GD-S1/Assets/Scripts/GameManager.cs
@@ -49,6 +49,6 @@ public class GameManager : MonoBehaviour
 
     private void LoadScene(SCENE_TYPE scene)
     {
-        //call in scene manager
+        m_SceneManager.LoadScene(scene);
     }
 }
--- GD-S1/Assets/Scripts/Scene_Manager.cs
+++ GD-S1/Assets/Scripts/Scene_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum SCENE_TYPE
 {
@@ -12,6 +13,18 @@ public enum SCENE_TYPE
 
 public class Scene_Manager : MonoBehaviour
 {
+    public void LoadScene(SCENE_TYPE scene)
+    {
+        if(scene == SCENE_TYPE.QUIT_GAME)
+        {
+            QuitApplication();
+        }
+        else
+        {
+            //SceneManager.LoadSceneAsync(scene.ToString(), LoadSceneMode.Single);
+        }
+    }
+
     private void QuitApplication()
     {
         Application.Quit();
