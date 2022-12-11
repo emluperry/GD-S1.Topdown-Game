2f3fbc8298c65cbedb50e39dc475e385ac32d9d3
emluperry <emluperry@gmail.com>
2022-12-11 16:36:52 +0000

    Revert "Added Audio"
    This reverts commit 66998ee113ee8d4f9f4b48989cb5613372f8c7d9.

--- GD-S1/Assets/Scripts/Enemies/Enemy_Handler.cs
+++ GD-S1/Assets/Scripts/Enemies/Enemy_Handler.cs
@@ -8,13 +8,7 @@ public class Enemy_Handler : MonoBehaviour
     private Enemy_Movement m_Movement;
     private Enemy_Animation m_Animation;
     private Entity_Health m_Health;
-<<<<<<< Updated upstream
     private UI_Healthbar m_UIHealthbar;
-=======
-    private Enemy_SFX m_EnemySFX;
-    private UI_SegmentBar m_UIHealthbar;
-    private Collider2D m_Collider;
->>>>>>> Stashed changes
 
     private bool m_IsAlive = true;
 
@@ -25,24 +19,14 @@ public class Enemy_Handler : MonoBehaviour
         m_Movement = GetComponent<Enemy_Movement>();
         m_Animation = GetComponent<Enemy_Animation>();
         m_Health = GetComponent<Entity_Health>();
-<<<<<<< Updated upstream
         m_UIHealthbar = GetComponentInChildren<UI_Healthbar>();
-=======
-        m_EnemySFX = GetComponent<Enemy_SFX>();
-        m_UIHealthbar = GetComponentInChildren<UI_SegmentBar>();
-        m_Collider = GetComponent<Collider2D>();
->>>>>>> Stashed changes
 
         m_Movement.UpdateInput += m_Animation.SetInputDirection;
         m_Movement.ChargingAttack += m_Animation.SetChargingBool;
         m_Movement.Attack += m_Animation.SetAttackTrigger;
 
         m_Health.Killed += SlimeKilled;
-<<<<<<< Updated upstream
         m_Health.DamageTaken += m_UIHealthbar.UpdateHealth;
-=======
-        m_Health.DamageUpdated += TakeDamage;
->>>>>>> Stashed changes
 
         m_Health.Destroyable += SetInactive;
     }
@@ -58,12 +42,6 @@ public class Enemy_Handler : MonoBehaviour
         m_Health.Destroyable -= SetInactive;
 
         gameObject.SetActive(false);
-    }
-
-    private void TakeDamage(float dec)
-    {
-        m_UIHealthbar.UpdateValue(dec);
-        m_EnemySFX.PlayHurtSFX();
     }
 
     private void SlimeKilled()
